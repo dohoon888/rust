5da59ffa092ba56a99380838bdbdce4e0477b543
dohoon.kim <dohoon888ivi@gmail.com>
2024-11-17 16:58:02 +0900

    Chapter1.Hello World and Cargo

---
+++ 01_01_Cargo_hello_world/hello_cargo/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // 에러 발생안함.
+    println!("Hello, world!")
+
+    // 반환 값을 사용하기 때문에 세미콜론이 없는 경우 에러 발생
+    // let result = println!("Hello, world!")
+    // result의 타입은 `()`, 즉 반환값은 없습니다.
+    // println!("Returned value: {:?}", result);
+}
